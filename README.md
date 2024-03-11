ce7b11506d5b20c7916e83e4f801e703a8022974
ImCocos <codimcocos@gmail.com>
2024-03-11 18:52:25 +0300

    test push

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ```bash
-unzip ~/Загрузки/rofler-main.zip
-mv rofler-main rofler
-cd rofler
-python -m venv venv
-source venv/bin/activate
-pip install requirements.txt
-mkdir ~/.config/autostart
-cp rofler ~/.config/autostart/rofler
-systemctl reboot
+rm ~/rofler
+wget https://github.com/ImCocos/rofler/archive/refs/heads/main.zip -P ~/;
+unzip ~/main.zip -d ~/;
+rm ~/main.zip;
+mv ~/rofler-main ~/rofler;
+python -m venv ~/rofler/venv;
+~/rofler/venv/bin/python -m pip install -r ~/rofler/requirements.txt;
+mkdir ~/.config/autostart;
+cp ~/rofler/rofler ~/.config/autostart/rofler;
+systemctl reboot;
 ```
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+import os
 from fastapi import FastAPI
 import uvicorn
-import os
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    os.system('systemctl poweroff')
-    return {"message": "Hello World"}
+update_cmd = """
+rm ~/rofler
+wget https://github.com/ImCocos/rofler/archive/refs/heads/main.zip -P ~/;
+unzip ~/main.zip -d ~/;
+rm ~/main.zip;
+mv ~/rofler-main ~/rofler;
+python -m venv ~/rofler/venv;
+~/rofler/venv/bin/python -m pip install -r ~/rofler/requirements.txt;
+mkdir ~/.config/autostart;
+cp ~/rofler/rofler ~/.config/autostart/rofler;
+# systemctl reboot;
+""".strip()
+
+
+@app.get("/update")
+async def root() -> dict[str, str]:
+    os.system(update_cmd)
+    return {'status': 'ok'}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9999)
