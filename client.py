e51eb67eac0c5cb3978636c262b92e9d634ed6f2
ImCocos <90627867+ImCocos@users.noreply.github.com>
2024-03-17 01:31:59 +0300

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ EXIT = threading.Event()
 
 
 def kill_client() -> None:
-    os.system('ps -aux > /home/imcocos/.processes.tmp')
+    os.system('ps -aux > /home/student/rofler/.processes.tmp')
 
     with open('/home/student/rofler/.processes.tmp', 'r') as file:
         processes = file.read().strip().split('\n')[1:]
